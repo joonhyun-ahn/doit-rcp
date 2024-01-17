346bcbc1e4a5a2d8d898984263ddb99f275751cc
joonhyun <joonhyun.ahn@apptest.ai>
2024-01-17 23:11:12 +0900

    ch03_2

---
+++ ch03/ch03_2/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import TailWindcss from './pages/TailWindcss';
+import Color from './pages/Color';
+import TextsTest from './pages/TextsTest';
+
+function App() {
+  return (
+    <div>
+      <TextsTest />
+      <Color />
+      <TailWindcss />
+    </div>
+  );
+}
+
+export default App;
---
+++ ch03/ch03_2/src/pages/TextsTest.tsx
@@ -0,0 +1,3 @@
+export default function TextsTest() {
+  return <div></div>;
+}
