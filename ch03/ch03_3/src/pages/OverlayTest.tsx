6381e5b4128740df788caaa317a9294f0f4b1751
joonhyun-ahn <joonhyun.ahn@gmail.com>
2024-01-21 15:36:37 +0900

    ch03_3

--- ch03/ch03_3/src/pages/OverlayTest.tsx
+++ ch03/ch03_3/src/pages/OverlayTest.tsx
@@ -1,10 +1,17 @@
-import { Title } from '../components';
+import { Div, Icon, Overlay, Title } from '../components';
 
 export default function OverlayTest() {
   return (
     <section className="mt-4">
       <Title>OverlayTest</Title>
-      <div className="mt-4"></div>
+      <Overlay opacityClass="bg-black-70">
+        <Div className="relative flex items-center justify-center p-8 bg-white h-1/2">
+          <Div className="absolute" right="1rem" top="1rem">
+            <Icon name="close" className="text-gray-500" />
+          </Div>
+          <p className="text-5xl">modal dialog box</p>
+        </Div>
+      </Overlay>
     </section>
   );
 }
