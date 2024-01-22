be13ed7c547f9a064dd53a2eee1c7b56830e0769
joonhyun <joonhyun.ahn@apptest.ai>
2024-01-22 22:19:12 +0900

    ch03_5

---
+++ ch03/ch03_5/src/App.tsx
@@ -0,0 +1,15 @@
+import ButtonTest from './pages/ButtonTest';
+import InputTest from './pages/InputTest';
+import ModalTest from './pages/ModalTest';
+
+function App() {
+  return (
+    <main>
+      <ButtonTest />
+      <InputTest />
+      <ModalTest />
+    </main>
+  );
+}
+
+export default App;
---
+++ ch03/ch03_5/src/pages/ModalTest/index.tsx
@@ -0,0 +1,8 @@
+export default function ModalTest() {
+  return (
+    <section className="mt-4">
+      <h2 className="font-bold text-5xl text-center">ModalTest</h2>
+      <div className="mt-4"></div>
+    </section>
+  );
+}
