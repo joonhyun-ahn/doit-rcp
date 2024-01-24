b443583e64e317f87cc018d9fb76072513f65c39
joonhyun <joonhyun.ahn@apptest.ai>
2024-01-24 14:51:07 +0900

    ch03_5 final

--- ch03/ch03_5/src/pages/ModalTest/index.tsx
+++ ch03/ch03_5/src/pages/ModalTest/index.tsx
@@ -1,8 +1,29 @@
+import { Subtitle, Title } from '../../components';
+import * as D from '../../data';
+import { Button, Modal, ModalAction, ModalContent } from '../../theme/daisyui';
+
 export default function ModalTest() {
+  const open = true;
+  const closeClicked = () => alert('closeClicked');
+  const acceptClicked = () => alert('acceptClicked');
+
   return (
     <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">ModalTest</h2>
-      <div className="mt-4"></div>
+      <Title>ModalTest</Title>
+      <Modal open={open}>
+        <ModalContent onCloseIconClicked={closeClicked}>
+          <Subtitle>Modal</Subtitle>
+          <p className="mt-4 text-justify">{D.randomParagraphs()}</p>
+          <ModalAction>
+            <Button className="w-24 normal-case btn-primary btn-sm" onClick={acceptClicked}>
+              Accept
+            </Button>
+            <Button className="w-24 normal-case btn-sm" onClick={closeClicked}>
+              Close
+            </Button>
+          </ModalAction>
+        </ModalContent>
+      </Modal>
     </section>
   );
 }
