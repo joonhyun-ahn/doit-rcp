303b61e452d9764c46e376a4355db2144726b111
joonhyun-ahn <joonhyun.ahn@gmail.com>
2024-02-05 05:30:27 +0900

    ch05_2

---
+++ ch05/ch05_2/src/components/Overlay.tsx
@@ -0,0 +1,16 @@
+import type { FC } from 'react';
+import { Div, type ReactDivProps } from './Div';
+
+export type OverlayProps = ReactDivProps & {
+  opacityClass?: string;
+};
+
+export const Overlay: FC<OverlayProps> = ({ className: _className, opacityClass, children, ...props }) => {
+  const className = [_className, 'absolute z-50 w-screen h-screen', opacityClass ?? 'bg-black/70', 'flex items-center justify-center'].join(' ');
+
+  return (
+    <Div {...props} className={className} top="0" left="0">
+      {children}
+    </Div>
+  );
+};
--- ch05/ch05_2/src/store/useStore.ts
+++ ch05/ch05_2/src/store/useStore.ts
@@ -1,11 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
 import { useMemo } from 'react';
 import { rootReducer } from './rootReducer';
-
-// resux package 버전 영향 받음
-// @reduxjs/toolkit : ^1.8.5 -> configureStore() middleware 오류 해결
-// react-redux : ^8.0.2
-// redux : ^4.2.0
+import { configureStore } from '@reduxjs/toolkit';
 
 const initializeStore = () => {
   const store = configureStore({ reducer: rootReducer, middleware: [] });
